21a917a14af62e9a46710e82e9344d32ebd49a2e
FSTivane <ciscotivane@gmail.com>
2023-08-24 16:22:24 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - oncatenate 2 strings
+ * @dest: destination file
+ * @src: source
+ * @n: number of bytes
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}	
